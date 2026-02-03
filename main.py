3d9645793c3856ed891cf2f79dcb4e6de7f42b0e
Wayne-cyw <wayne.chiu0823@gmail.com>
2026-02-03 01:24:16 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ Gold Price Prediction model using Random Forest Regressor.
 This script loads gold price data, preprocesses it, trains a Random Forest Regressor model,
 and evaluates its performance on a test dataset.
 """
+import pandas as pd
+import numpy as np
 
