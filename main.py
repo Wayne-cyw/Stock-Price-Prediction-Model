faa2f7e911d274422c9a3d049ce3411348e567ff
Wayne Chiu <wayne.chiu0823@gmail.com>
2026-02-04 16:00:24 -0500

    Revert "Finished model"

--- main.py
+++ main.py
@@ -5,10 +5,4 @@ and evaluates its performance on a test dataset.
 """
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn import metrics
-
 
